13be2eeceb5eb81b490922f40c784f82061a52e3
Jovana <125403554+J0vana23@users.noreply.github.com>
2024-05-19 14:13:01 -0300

    Update tarefa4.kt

--- Exercicios/tarefa4.kt
+++ Exercicios/tarefa4.kt
@@ -11,17 +11,16 @@ data class Event(
 
 fun main() {
     val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, duration = 0)
-	val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, duration = 15)
-	val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, duration = 30)
-	val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, duration = 60)
-	val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, duration = 10)
-	val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, duration = 45)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, duration = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, duration = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, duration = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, duration = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, duration = 45)
 
-val Eventos = mutableListOf<Event>(event1,event2,event3,event4,event5,event6)
+    val Eventos = mutableListOf<Event>(event1,event2,event3,event4,event5,event6)
     
-val eventosCurtos = Eventos.filter { it.duration < 60 }
+    val eventosCurtos = Eventos.filter { it.duration < 60 }
 
-println("Você tem ${eventosCurtos.size} eventos curtos.")
-}
- 
+   println("Você tem ${eventosCurtos.size} eventos curtos.")
+ } 
 }
