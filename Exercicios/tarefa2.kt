3f01bf6f157db42e0a6cbbac9aaf8083348a050c
Jovana <125403554+J0vana23@users.noreply.github.com>
2024-05-21 13:18:19 -0300

    Exercícios

--- Exercicios/tarefa2.kt
+++ Exercicios/tarefa2.kt
@@ -9,6 +9,7 @@ data class Event(
     val duration: Int,
 )
 
+
 fun main() {
     val evento = Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart=Daypart.morning, duration=15)
     println("Title: "+evento.title)
